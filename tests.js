17c1980c21084e885c1dc3131a6cd18153a2521e
Tim Waszak <tmw8633@truman.edu>
2013-04-17 15:55:54 -0500

    Made drawBox button and function.

--- tests.js
+++ tests.js
@@ -1,3 +1,18 @@
 test( "Canvas Test", function() {
-  ok( document.getElementById("mainCanvas"), "Passed!" );
+  ok( document.getElementById('mainCanvas'), "Passed!" );
+  
+});
+
+test( "Canvas Test", function() {
+  ok( document.getElementById('boxButton'), "Button!" );
+  
+});
+
+test("rectangle!!!", function() {
+ var $button = $("boxButton");
+	$button.on( "click", function() {
+		ok( true, "button was clicked!");
+		});
+		
+	$button.trigger("click");
 });
