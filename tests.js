cfb99aecbb327695bada1653cf9c2b7757409be1
David Wimmer <dlwimmer@newcommunities.net>
2013-04-23 19:03:52 -0500

    Removed fake tests. Updated test html for new libraries.

--- tests.js
+++ tests.js
@@ -1,37 +1,24 @@
+var stage;
+var layer;
+
+module( "Canvas", {
+	setup: function() {
+		stage = new Kinetic.Stage({
+        	container: 'stage-container',
+        	width: 1024,
+        	height: 768,
+        	draggable: true
+        });
+
+        layer = new Kinetic.Layer();
+      
+        stage.add(layer);
+	},
+	teardown: function() {
+		// clean up after each test
+	}
+});
+
 test( "Canvas Test", function() {
-  ok( document.getElementById('mainCanvas'), "Main Canvas" );
-  
-});
-
-test( "Button Test", function() {
-  ok( document.getElementById('boxButton'), "Box Button" );
-  
-});
-
-
-test( "New Box", 1, function() {
-	$("#mainCanvas").on("dragover", overDrag);
-	$("#mainCanvas").on("drop", function(e) {
-		drop(e);
-		ok(true, "Dropped Box");
-	});
-	
-	$("#mainCanvas").trigger("drop");
-
-})
-
-test("rectangle!!!", 1, function() {
- 	var button = $("#boxButton");
- 
-	button.on( "click", function() {
-		ok( true, "button was clicked!");
-	});
-		
-	button.trigger("click");
-
-});
-
-test( 'drawMyBezier()', function() {
-	ok( drawMyBezier(2, 3), "Bezier");
-	//not really a test... more proof of drawing
+	ok( stage, "does stage exist?" );  
 });
